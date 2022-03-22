62091a0a98618c54181ce292aaaea6482799aeb1
R <streltssov@gmail.com>
2022-03-22 19:28:09 +0500

    rewrite sliderView to class

--- src/sliderView.ts
+++ src/sliderView.ts
@@ -1,73 +1,103 @@
 import { init, sliderLogic } from '../src/sliderController';
 
-window.addEventListener('DOMContentLoaded', () => {
-  const upper: HTMLElement = document.querySelector('.slider__handle-upper');
-  const lower: HTMLElement = document.querySelector('.slider__handle-lower');
-  const slider: HTMLElement = document.querySelector('.slider');
-  const valueLower: HTMLElement = document.querySelector('.slider__values-lower');
-  const valueUpper: HTMLElement = document.querySelector('.slider__values-upper');
-  const sliderLeftCoord = slider.getBoundingClientRect().left;
-  const sliderRightCoord = slider.getBoundingClientRect().right;
+class SliderView {
 
-  let currentLowValue = 0;
-  let currentUpperValue = init.sliderLength;
+  slider: HTMLElement;
 
-  function progressHighlight(minCoord: number, maxCoord: number) {
-    const sliderHighlight: HTMLElement = document.querySelector('.slider__highlight');
+  currentLowValue: number;
+
+  currentUpperValue: number;
+
+  constructor(slider: HTMLElement, sliderLength: number) {
+    this.slider = slider;
+    this.currentLowValue = 0;
+    this.currentUpperValue = sliderLength;
+  }
+
+  findElem(selector: string) {
+    const elems = document.querySelectorAll(selector);
+    let targetElem: HTMLElement;
+    
+    elems.forEach((elem: HTMLElement) => {
+      if (elem.parentNode === this.slider) {
+        targetElem = elem;
+      }
+    });
+    
+    return targetElem;
+  }
+
+  progressHighlight(minCoord: number, maxCoord: number) {
+    const sliderHighlight: HTMLElement = this.findElem('.slider__highlight');
     const progressLength = maxCoord - minCoord;
     if ( progressLength >= 0 ) {
       sliderHighlight.style.width = progressLength + 'px';
-      sliderHighlight.style.left = minCoord - slider.getBoundingClientRect().left + 'px';
+      sliderHighlight.style.left = minCoord - this.slider.getBoundingClientRect().left + 'px';
     } else {
       sliderHighlight.style.width = 0 + 'px';
-    }  
+    }
+  } 
+
+  showValues() {
+    document.querySelector('.slider__values-lower').innerHTML = sliderLogic.toCustomValue(this.currentLowValue).toString();
+    document.querySelector('.slider__values-upper').innerHTML = sliderLogic.toCustomValue(this.currentUpperValue).toString();
   }
 
-  function getValues() {
-    valueLower.innerHTML = sliderLogic.toCustomValue(currentLowValue).toString();
-    valueUpper.innerHTML = sliderLogic.toCustomValue(currentUpperValue).toString();
-  }
-
-  function shift(elem: HTMLElement, e: MouseEvent) {
-    
+  shift(elem: HTMLElement, e: MouseEvent) {
+    const lower = this.findElem('.slider__handle-lower');
+    const upper = this.findElem('.slider__handle-upper');
     const minCoord = lower.getBoundingClientRect().right;
     const maxCoord = upper.getBoundingClientRect().left;
-
+    const sliderLeftCoord = this.slider.getBoundingClientRect().left;
+    const sliderRightCoord = this.slider.getBoundingClientRect().right;
+  
     let minCoordShift = minCoord - lower.offsetWidth / 2 - sliderLeftCoord;
     let maxCoordShift = maxCoord - sliderLeftCoord - lower.offsetWidth / 2;
-
+    
     if (elem === lower) {
       minCoordShift = 0;
       maxCoordShift = maxCoord + lower.offsetWidth / 2 - sliderLeftCoord;
-      currentLowValue = sliderLogic.checkExtremumCoords(e.pageX - sliderLeftCoord, minCoordShift, maxCoordShift);
+      this.currentLowValue = sliderLogic.checkExtremumCoords(e.pageX - sliderLeftCoord, minCoordShift, maxCoordShift);
       lower.style.zIndex = '10';
       upper.style.zIndex = '1';
+      elem = lower;    
     }
     if (elem === upper) {
       minCoordShift = minCoord - upper.offsetWidth - sliderLeftCoord + upper.offsetWidth / 2;
       maxCoordShift = sliderRightCoord - sliderLeftCoord;
-      currentUpperValue = sliderLogic.checkExtremumCoords(e.pageX - sliderLeftCoord, minCoordShift, maxCoordShift);
+      this.currentUpperValue = sliderLogic.checkExtremumCoords(e.pageX - sliderLeftCoord, minCoordShift, maxCoordShift);
       upper.style.zIndex = '10';
       lower.style.zIndex = '1';
+      elem = upper;
     }
+    
     elem.style.left = sliderLogic.checkExtremumCoords(e.pageX - sliderLeftCoord, minCoordShift, maxCoordShift) + 'px';
-    getValues();
-    progressHighlight(minCoord, maxCoord);
+    this.showValues();
+    this.progressHighlight(minCoord, maxCoord);
   }
-
-  function addEvents(elem: HTMLElement) {
-    const bindShift = shift.bind(null, elem);
+  
+  addEvents(elem: HTMLElement) {
+    const bindShift = this.shift.bind(this, elem);
+    
     document.addEventListener('mousemove', bindShift);
     document.addEventListener('mouseup', () => {
       document.removeEventListener('mousemove', bindShift);
-      elem.onmouseup = null;
+      document.onmouseup = null;
     });
-    elem.ondragstart = function () {
+    document.ondragstart = function () {
       return false;
     };
   }
-  getValues();
 
-  upper.addEventListener('mousedown', () => addEvents(upper));
-  lower.addEventListener('mousedown', () => addEvents(lower));
-});
+  init() {
+    const lower = this.findElem('.slider__handle-lower');
+    const upper = this.findElem('.slider__handle-upper');
+    this.showValues();
+    upper.addEventListener('mousedown', () => this.addEvents(upper));
+    lower.addEventListener('mousedown', () => this.addEvents(lower));
+    
+  }
+}
+
+const sliderView = new SliderView(document.querySelector('.slider'), init.sliderLength);
+sliderView.init();
